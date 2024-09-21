19a7477d39acf8f7478dbcdb849b79f124aba41a
jckstllrd <jackstallard17@gmail.com>
2024-09-21 10:10:00 +0100

    Add gameBoard functionality

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const gameController = function () {
+    const playGame = () => ()
   return;
 };
+
+const gameboard = function () {
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  const updateBoard = (marker, index) => {board[index] = marker};
+  return board;
+};
+
+// const Player =
+
+// }
+
+gameController.startGame();
