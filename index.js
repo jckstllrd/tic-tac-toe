f148ffc7459e2e5f79b9e9bae27529b1d9c872a0
jckstllrd <jackstallard17@gmail.com>
2024-09-22 12:22:10 +0100

    Add winOrDraw function to check the state of the board

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const gameController = (function () {
-  let gameOver = false;
+  let winState = false;
+  let drawState = false;
 
   const initiatePlayer = () => {
     const playerName = prompt("Player Name:");
@@ -8,14 +9,39 @@ const gameController = (function () {
     return player;
   };
 
+  const initiateComputer = (playerMarker) => {
+    let computerMarker = "";
+    if (playerMarker == "X") {
+      computerMarker = "O";
+    } else {
+      computerMarker = "X";
+    }
+    const compName = "Computer";
+    const computer = createPlayer(compName, computerMarker);
+    return computer;
+  };
+
+  const winOrDraw = () => {
+    [winState, drawState] = gameboard.boardStateCheck();
+    if (winState) {
+      console.log("We have a winner");
+    } else if (drawState) {
+      console.log("We have a draw");
+    }
+  };
+
   const playGame = () => {
     const player1 = initiatePlayer();
+    const computer = initiateComputer(player1.getMarker());
+    computer.updateTurn(false);
     gameboard.displayBoard();
-    while (!gameOver) {
+    while (!winState && !drawState) {
       console.log(player1.getTurn());
 
       console.log("here 1");
       if (player1.getTurn()) {
+        console.log("Player turn");
+
         console.log("here 2");
         let playerSelectionRow = prompt("Pick a row 1-3:");
         let playerSelectionCol = prompt("Pick a column 1-3:");
@@ -25,8 +51,29 @@ const gameController = (function () {
           playerSelectionCol
         );
         player1.updateTurn(false);
+        computer.updateTurn(true);
+        winOrDraw();
+        if (winState || drawState) {
+          break;
+        }
+      } else if (computer.getTurn()) {
+        console.log("computer turn");
+
+        let computerSelection = "";
+        let selectRow;
+        let selectCol;
+        while (computerSelection != "_") {
+          selectRow = Math.floor(Math.random() * 3 + 1);
+          selectCol = Math.floor(Math.random() * 3 + 1);
+          console.log(selectRow, selectCol);
+
+          computerSelection = gameboard.checkCell(selectRow, selectCol);
+        }
+        gameboard.updateBoard(computer.getMarker(), selectRow, selectCol);
+        computer.updateTurn(false);
+        player1.updateTurn(true);
+        winOrDraw();
       }
-      gameOver = true;
     }
   };
   return { playGame };
@@ -44,15 +91,116 @@ const gameboard = (function () {
     displayBoard();
   };
   const displayBoard = () => {
-    console.log(board);
+    board.forEach((row) => {
+      console.log(row);
+    });
   };
 
-  return { updateBoard, displayBoard };
+  const checkCell = (row, col) => {
+    return board[row - 1][col - 1];
+  };
+
+  const boardStateCheck = () => {
+    let isWinner = false;
+    let isDraw = false;
+
+    let filledCount = 0;
+
+    //Rows
+    for (let x = 0; x < board.length; x++) {
+      let prevCell = "";
+      let matches = 1;
+      let row = board[x];
+      for (let y = 0; y < board.length; y++) {
+        let cell = row[y];
+        console.log(cell + " vs. Previous: " + prevCell);
+
+        if (cell == "_") {
+          console.log("No winner on this row");
+          prevCell = cell;
+          break;
+        }
+        if (cell == prevCell) {
+          console.log("Same cell");
+
+          matches++;
+        }
+        prevCell = cell;
+        filledCount++;
+      }
+
+      if (matches == 3) {
+        isWinner = true;
+        break;
+      }
+    }
+
+    // Columns
+
+    if (!isWinner || !isDraw) {
+      for (let x = 0; x < board.length; x++) {
+        let prevCell = "";
+        let matches = 1;
+        for (let y = 0; y < board.length; y++) {
+          let cell = board[y][x];
+          console.log(cell + " vs. Previous: " + prevCell);
+
+          if (cell == "_") {
+            console.log("No winner on this row");
+            prevCell = cell;
+            break;
+          }
+          if (cell == prevCell) {
+            console.log("Same cell");
+
+            matches++;
+          }
+          prevCell = cell;
+        }
+
+        if (matches == 3) {
+          isWinner = true;
+          break;
+        }
+      }
+    }
+
+    // First diagonal
+    if (!isWinner && !isDraw) {
+      let firstCell = board[0][0];
+      let secondCell = board[1][1];
+      let thirdCell = board[2][2];
+
+      if (firstCell == secondCell && secondCell == thirdCell) {
+        isWinner = true;
+      }
+    }
+
+    // Second diagonal
+    if (!isWinner && !isDraw) {
+      let firstCell = board[0][2];
+      let secondCell = board[1][1];
+      let thirdCell = board[2][1];
+
+      if (firstCell == secondCell && secondCell == thirdCell) {
+        isWinner = true;
+      }
+    }
+
+    if (filledCount == 9 && !isWinner) {
+      isDraw = true;
+    }
+    console.log("Filled = " + filledCount);
+
+    return [isWinner, isDraw];
+  };
+
+  return { updateBoard, displayBoard, boardStateCheck, checkCell };
 })();
 
 function createPlayer(name, marker) {
   const score = 0;
-  const isTurn = true;
+  let isTurn = true;
   const getMarker = () => marker;
   const getScore = () => score;
   const getTurn = () => isTurn;
